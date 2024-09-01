87e3ea0e707435be9607df1b0d6a5647f541c3b3
Shento Hendriks <shento@shentohendriks.nl>
2024-09-01 22:25:49 +0200

    added gap + no cache

--- app/components/RecipesGrid/RecipesGrid.js
+++ app/components/RecipesGrid/RecipesGrid.js
@@ -1,16 +1,20 @@
 export default async function RecipesGrid() {
   let data = await fetch(
-    "https://recepten-backend.shentostaging.nl/items/recepten"
+    "https://recepten-backend.shentostaging.nl/items/recepten",
+    { cache: "no-store" }
   );
   let recepten = await data.json();
 
   return (
     <>
       <h2 className="text-2xl mt-8 mb-4">Nieuwste Recepten</h2>
-      <section className="grid grid-cols-3">
+      <section className="grid grid-cols-3 gap-4">
         {recepten.data.map((recept) => {
           return (
-            <div className="aspect-square bg-gray-500 text-white p-8" key={recept.id}>
+            <div
+              className="aspect-square bg-gray-500 text-white p-8"
+              key={recept.id}
+            >
               <h3 className="text-2xl font-bold">{recept.name}</h3>
             </div>
           );
