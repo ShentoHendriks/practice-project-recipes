fc78a133549ea98c47ea85a0eb6ab6e37998f2f0
Shento Hendriks <shento@shentohendriks.nl>
2024-09-01 19:05:45 +0200

    added text

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <>
       <h1 className="text-4xl font-bold tracking-tighter container p-8">Hello Welcome to my beautiful app</h1>
+      <p>this is a test</p>
     </>
   );
 }
