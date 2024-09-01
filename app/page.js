a2d4fc504035e4445ab7509a51c52f09230fa82b
Shento Hendriks <shento@shentohendriks.nl>
2024-09-01 18:59:27 +0200

    first commit

---
+++ app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold tracking-tighter container p-8">Hello Welcome to my beautiful app</h1>
+    </>
+  );
+}
